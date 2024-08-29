31fc7412e6ebfc4f224b39c1597d0c686700d858
bsandipan88 <bsandipan88@gmail.com>
2024-08-29 18:02:55 +0530

    Pushing HelloWorld to repo

---
+++ HelloWorld/HelloWorld/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HelloWorld
+//
+//  Created by Sandipan Banerjee on 29/08/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
